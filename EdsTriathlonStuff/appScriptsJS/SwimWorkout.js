06d54846c175920809304a1b3eaec7c4902dbb26
Edward Sproull <ecs3@po.cwru.edu>
2019-10-23 14:40:38 -0700

    JS to TS

--- EdsTriathlonStuff/appScriptsJS/SwimWorkout.js
+++
@@ -1,8 +0,0 @@
-function Welcome(person) {
-    return "<h2>Hello " + person + ", Lets learn TypeScript</h2>";
-}
-function ClickMeButton() {
-    var user = "MithunVP";
-    document.getElementById("divMsg").innerHTML = Welcome(user);
-}
-//# sourceMappingURL=SwimWorkout.js.map
--- EdsTriathlonStuff/wwwroot/appScriptsJS/SwimWorkout.js
+++ EdsTriathlonStuff/wwwroot/appScriptsJS/SwimWorkout.js
@@ -1,44 +1,35 @@
-﻿$(function () {
-    var poolLength: number = 25;
-    var yards: boolean = true;
-    var dialogTitle: string = "";
-    var workoutId: string = "";
-
-    $(".poollengthinput").on("change", function (event: Event) {
-        var parts = (<HTMLInputElement>event.currentTarget).value.split(' ');
+$(function () {
+    var poolLength = 25;
+    var yards = true;
+    var dialogTitle = "";
+    var workoutId = "";
+    $(".poollengthinput").on("change", function (event) {
+        var parts = event.currentTarget.value.split(' ');
         poolLength = parseInt(parts[0]);
         if (poolLength === 33) {
             poolLength = 33.33333;
-
         }
-
         yards = parts[1] == "Yards";
         UpdateYardage(true);
     });
-
-    $(".addvalue").on("click", function(event: Event): boolean {
-        var index: string = (<HTMLInputElement>event.currentTarget).getAttribute("data_index");
-        var newitem: string = <string>$("#newOptionTxt" + index).val(); //get the new value
-
-        var newInt: number = parseInt(newitem);
-        var mod: number = newInt % poolLength
+    $(".addvalue").on("click", function (event) {
+        var index = event.currentTarget.getAttribute("data_index");
+        var newitem = $("#newOptionTxt" + index).val(); //get the new value
+        var newInt = parseInt(newitem);
+        var mod = newInt % poolLength;
         if (mod > 1 && poolLength - mod > 1) {
             alert("Not legit for pool size");
             return true;
         }
-
-        var selectLists: HTMLCollectionOf<HTMLSelectElement> =
-            (<HTMLCollectionOf<HTMLSelectElement>>document.getElementsByClassName("distselect"));
+        var selectLists = document.getElementsByClassName("distselect");
         for (var i = 0; i < selectLists.length; i++) {
-            var newOption: HTMLOptionElement = <HTMLOptionElement>window.document.createElement("OPTION");
+            var newOption = window.document.createElement("OPTION");
             newOption.text = newitem;
             newOption.value = newitem;
-
             for (var j = 0; j < selectLists[i].options.length; j++) {
                 if (newInt == parseInt(selectLists[i].options[j].value)) {
                     break;
                 }
-
                 if (parseInt(newOption.value) < parseInt(selectLists[i].options[j].value) ||
                     selectLists[i].options[j].text == "Other") {
                     selectLists[i].options.add(newOption, selectLists[i].options[j]);
@@ -46,63 +37,54 @@
                 }
             }
         }
-
         $("#div_content" + index).css("display", "none");
         var distId = "dist" + index;
-        (<HTMLInputElement>document.getElementById(distId)).value = newOption.value;
-        return false
+        document.getElementById(distId).value = newOption.value;
+        return false;
     });
-
     $(".poollengthinput").on("change", function (event) {
-        var parts = (<HTMLInputElement>event.currentTarget).value.split(' ');
+        var parts = event.currentTarget.value.split(' ');
         poolLength = parseInt(parts[0]);
         if (poolLength === 33) {
             poolLength = 33.33333;
-
         }
-
         yards = parts[1] == "Yards";
         UpdateYardage(true);
     });
-
     $("#print").click(function printSwimWorkout() {
-        var headstr: string = "<html><head><title></title></head><body>";
-        var footstr: string = "</body>";
-        var newstr: string = (<HTMLElement>document.all.item("swimout")).innerHTML;
+        var headstr = "<html><head><title></title></head><body>";
+        var footstr = "</body>";
+        var newstr = document.all.item("swimout").innerHTML;
         console.log("%O", document.all.item("swimout"));
         console.log("%O", newstr);
-
-        var oldstr: string = document.body.innerHTML;
+        var oldstr = document.body.innerHTML;
         document.body.innerHTML = headstr + newstr + footstr;
         window.print();
         document.body.innerHTML = oldstr;
         return false;
     });
-
     $(".repsentry").on("change", function (event) {
-        var val: string  = (<HTMLInputElement>event.currentTarget).value;
-        var i: number = parseInt(event.currentTarget.getAttribute("data-index"));
+        var val = event.currentTarget.value;
+        var i = parseInt(event.currentTarget.getAttribute("data-index"));
         UpdateSet(i);
         UpdateYardage(false);
     });
-
     $(".distselect").change(function (event) {
-        var value = (<HTMLInputElement>event.currentTarget).value; //get the selected option value
-        var index: number = parseInt(event.currentTarget.getAttribute("data-index"));
+        var value = event.currentTarget.value; //get the selected option value
+        var index = parseInt(event.currentTarget.getAttribute("data-index"));
         if (value === "other") {
             $("#div_content" + index).css("display", "block"); //display the add new dialog
-        } else {
+        }
+        else {
             UpdateSet(index);
             UpdateYardage(false);
         }
     });
-
     $("#dialog-1").dialog({
         autoOpen: false
     });
-
     $(".btn.btn-primary").contextmenu(function (e) {
-        var rect: ClientRect = e.currentTarget.getBoundingClientRect();
+        var rect = e.currentTarget.getBoundingClientRect();
         dialogTitle = e.currentTarget.innerText;
         workoutId = e.currentTarget.getAttribute("data_workoutid");
         $("#dialog-1").dialog({
@@ -117,87 +99,72 @@
         console.log("%O", e);
         e.preventDefault();
     });
-
     $("#delete").click(function (e) {
-        $.post("CreateWorkout/DeleteWorkout",
-            { Name: dialogTitle, Id: workoutId },
-            function (data) {
-                if (data == "success") {
-                    location.reload();
-                }
-            });
+        $.post("CreateWorkout/DeleteWorkout", { Name: dialogTitle, Id: workoutId }, function (data) {
+            if (data == "success") {
+                location.reload();
+            }
+        });
     });
-
     $("#rename").click(function (e) {
-        var newName: string = (<HTMLInputElement>$("#newname")[0]).value;
-        $.post("CreateWorkout/RenameWorkout",
-            { Name: newName, Id: workoutId },
-            function (data) {
-                if (data == "success") {
-                    location.reload();
-                }
-            });
+        var newName = $("#newname")[0].value;
+        $.post("CreateWorkout/RenameWorkout", { Name: newName, Id: workoutId }, function (data) {
+            if (data == "success") {
+                location.reload();
+            }
+        });
     });
-
-    function UpdateSet(i: number) {
-        var repsId: string = "reps" + i;
+    function UpdateSet(i) {
+        var repsId = "reps" + i;
         var distId = "dist" + i;
         var totId = "tot" + i;
-        var repsCount = parseInt((<HTMLInputElement>document.getElementById(repsId)).value);
-        var dist = parseFloat((<HTMLInputElement>document.getElementById(distId)).value);
+        var repsCount = parseInt(document.getElementById(repsId).value);
+        var dist = parseFloat(document.getElementById(distId).value);
         var tot = repsCount * dist;
-        (<HTMLInputElement>document.getElementById(totId)).value = Math.round(tot).toString();
+        document.getElementById(totId).value = Math.round(tot).toString();
     }
-
-    function UpdateYardage(poolSizeChanged: boolean) {
+    function UpdateYardage(poolSizeChanged) {
         var index = 0;
         if (poolSizeChanged) {
             var stdSetLengths = [1, 2, 3, 4, 5, 6, 8, 12, 16, 20, 32, 40];
-            var selectLists: HTMLCollectionOf<HTMLSelectElement> =
-                (<HTMLCollectionOf<HTMLSelectElement>>document.getElementsByClassName("distselect"));
+            var selectLists = document.getElementsByClassName("distselect");
             for (var i = 0; i < selectLists.length; i++) {
                 selectLists[i].options.length = 0;
-
                 stdSetLengths.forEach(function (lenghts) {
-                    var newOption: HTMLOptionElement =
-                        <HTMLOptionElement>window.document.createElement("OPTION");
+                    var newOption = window.document.createElement("OPTION");
                     var newItem = lenghts * poolLength;
                     newOption.text = Math.round(newItem).toString();
                     newOption.value = newItem.toString();
                     selectLists[i].options.add(newOption);
                 });
-
-                var newOption: HTMLOptionElement =
-                    <HTMLOptionElement>window.document.createElement("OPTION");
+                var newOption = window.document.createElement("OPTION");
                 newOption.text = "Other";
                 newOption.value = "Other";
                 selectLists[i].options.add(newOption);
-
-            };
+            }
+            ;
         }
-
         index = 0;
-        var total: number = 0;
-        var totId: string = "tot" + index;
-        var dist: HTMLInputElement = <HTMLInputElement>document.getElementById(totId);
+        var total = 0;
+        var totId = "tot" + index;
+        var dist = document.getElementById(totId);
         do {
             total += parseInt(dist.value, 10);
             totId = "tot" + ++index;
-            dist = <HTMLInputElement>document.getElementById(totId);
-        }
-        while (dist != null)
-
+            dist = document.getElementById(totId);
+        } while (dist != null);
         if (yards) {
-            (<HTMLInputElement>document.getElementById("yardstotal")).value =
+            document.getElementById("yardstotal").value =
                 Math.round(total).toString();
-            (<HTMLInputElement>document.getElementById("meterstotal")).value =
+            document.getElementById("meterstotal").value =
                 Math.round(0.9144 * total).toString();
         }
         else {
-            (<HTMLInputElement>document.getElementById("meterstotal")).value =
+            document.getElementById("meterstotal").value =
                 Math.round(total).toString();
-            (<HTMLInputElement>document.getElementById("yardstotal")).value =
+            document.getElementById("yardstotal").value =
                 Math.round(1.0936 * total).toString();
         }
     }
 });
+//# sourceMappingURL=SwimWorkout.js.map
